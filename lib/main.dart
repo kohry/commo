f83d424a8bde335ff160d3ea0725175641e68403
gorakgarak mackbook <kohry@gorakgarakui-MacBook-Pro.local>
2018-11-09 20:09:34 +0900

    ios build

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '컴모 - 커뮤니티 모아보기',
       home: MyHomePage(),
       theme: ThemeData(
